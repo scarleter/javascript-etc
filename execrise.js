9adb84f0ff86215a6832a954f442956678502d30
Mat.L <eric_op@163.com>
2017-03-07 15:12:40 +0800

    add remark

--- execrise.js
+++ execrise.js
@@ -1,3 +1,7 @@
+  //自建fn函數，輸入n得出數組為n的數組，該數組的值為2-32
+  //構建過程中發現了一下問題：function串錯，多次修改之後range名字換了不知道，遞歸返回不了數據是因為自己沒有在所有分支上加上return，
+  //同時後來發現當n 大於 range的範圍的時候，會報錯，所以要判斷n應該小於等於range[1]-range[0]
+  //修改n判斷，加入n為undefined的情況
   var gobal = {};
   (function(gobal){
 
@@ -12,6 +16,7 @@
 
           var range = [2,32];
           function generateRandom(range, len, obj, arr){
+              if(len>(range[1] - range[0])){//n大於range範圍
                   return [];
               } 
                
@@ -25,6 +30,7 @@
                       arr.push(r);
                   }
 
+                  return generateRandom(range, len, obj, arr); //一開始沒有加上return，所有call generateRandom 的時候返回undefined
               }
 
           }
