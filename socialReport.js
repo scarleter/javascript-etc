114b20935b9a7237a461429057c44d1abc8eee62
Mat.L <eric_op@163.com>
2017-03-11 23:56:17 +0800

    build Data.ajax, Data.get and Data.post

--- socialReport.js
+++ socialReport.js
@@ -1,28 +1,66 @@
 //socialReport.js
 
-//It has three subclassess: data, view and operation
+//It need jQuery.
+//It has three subclassess: Data, vVew and Operation
 
-//data class is designed to get the facebook and google data by ajax.
-//view class deal with UI 
-//operatioin class contain a pile of functions which help to calculate the date from the data class before render by view class
+//View class deal with UI 
+//Operatioin class contain a pile of functions which help to calculate the date from the data class before render by view class
 
-;(function(window){
-    window.socialReport = function(){
-        var socialReport = {};
+;(function($, window){
+    window.SocialReport = function(){
+        var SocialReport = {};
         
-        //data class
-        var data = socialReport.data = {};
+        //SocialReport.Data
+        //----------------
         
-        //view class
-        var view = socialReport.view = {};
+        //Data class is designed to get the facebook and google data by ajax.
+        var Data = SocialReport.Data = {};
         
-        //operation class
-        var operation = socialReport.operation = {};
+        //url for get data from server
+        Data.url = '';
+        
+        //wrap ajax
+        Data.ajax = function(data, options){
+            $.ajax({
+                url: options.url || Data.url,
+                type: options.type || 'GET',
+                data: data,
+                dataType: options.datatype || 'json',
+                success: options.success || '',
+                error: options.error || ''
+            });
+        };
+        
+        //get platform data from server by ajax.`type` can only be string or array.
+        //it would return in json.`platform` would be facebook by default.
+        Data.get = function(type, platform, options){
+            //make sure `type` is string or array
+            if(Object.prototype.toString.call( type ) !== '[object Array]' && typeof type !== 'string') return false;
+            platform = platform || 'facebook';
+            
+            Data.ajax({'type': type, 'platform': platform}, options || '');
+            
+        };
+        
+        //normally not call this function because we cannot update the platform data.
+        Data.post = function(type, platform, options){
+            //make sure `type` is string or array
+            if(Object.prototype.toString.call( type ) !== '[object Array]' && typeof type !== 'string') return false;
+            platform = platform || 'facebook';
+            
+            Data.ajax({'type': type, 'platform': platform}, options || '');
+        }
+        
+        //View class
+        var View = SocialReport.View = {};
+        
+        //Operation class
+        var Operation = SocialReport.Operation = {};
         
         
         
-        return socialReport;
+        return SocialReport;
     }();
     
     
-})(window);
+})($, window);
